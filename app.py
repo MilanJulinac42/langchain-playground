0aec3c05d17fe35e6ff43f4b1c846a12e31c3af7
MilanJulinac42 <milanjulinac996@gmail.com>
2024-12-10 13:19:42 +0100

    T1. removed comments

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Access variables
 api_key = os.getenv("API_KEY")
 secret_key = os.getenv("SECRET_KEY")
 
